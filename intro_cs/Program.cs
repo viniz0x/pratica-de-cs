0d79f783c9b3e4749a914ad6055a15b95f0d4460
viniz0x <armazenamentovini@gmail.com>
2024-08-18 14:43:16 -0300

    usando_classe

--- intro_cs/Program.cs
+++ intro_cs/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using intro_cs.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Vini";
+pessoa1.Idade = 20;
+pessoa1.Apresentar();
